9fe8983c5e24c7d2789d374f5eb6b8e8ad263557
Carlos Litwiñiuk <repropel78@gmail.com>
2022-04-11 07:36:36 +0200

    reto probado

--- list_comperhetion.py
+++ list_comperhetion.py
@@ -5,7 +5,7 @@ def run():
     #print(squeares)
 
     #Reto2
-    multiplos = [i*4 for i in range(1,10000) if i%6 == 0 and i%9 == 0]
+    multiplos = [i for i in range(1,100000) if i%4 == 0 and i%6 == 0 and i%9 == 0]
     print(multiplos)
 
 if __name__ == '__main__':
