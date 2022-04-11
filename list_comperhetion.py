294cf86a5d7b8cb1e8269a62a05bb560774d6c11
Carlos Litwiñiuk <repropel78@gmail.com>
2022-04-11 07:26:41 +0200

    reto2

--- list_comperhetion.py
+++ list_comperhetion.py
@@ -1,7 +1,12 @@
 def run():
-    squeares = [i**2 for i in range(1,101) if i%3 != 0]
-    print(squeares)
 
+    #Reto1
+    #squeares = [i**2 for i in range(1,101) if i%3 != 0]
+    #print(squeares)
+
+    #Reto2
+    multiplos = [i**4 for i in range(1,100000) if i%6 == 0]
+    print(multiplos)
 
 if __name__ == '__main__':
     run()
